c21feaf5e62799b27cecb922e3a68ae7dd38cb49
John Glenn Eligio <glenneligio@gmail.com>
2022-05-15 11:28:14 +0800

    Initial Commit

---
+++ backend/user-service/src/test/java/com/eze/userservice/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eze.userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
